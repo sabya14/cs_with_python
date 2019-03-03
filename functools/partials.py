8426ad5f9aaa1ba07641185e418fb1a21222da64
Sabyasachi Nandy <=>
2019-03-03 21:09:35 +0530

    Learnt about using partials.

--- functools/partials.py
+++ functools/partials.py
@@ -15,6 +15,11 @@ def show_details(name, func, is_partial=False):
         print(func.__name__)
     else:
         print("Partial")
+        try:
+            print(func.__name__)
+        except AttributeError as e:
+            print("If we dont use update wrapper, partial does not get the properties of func name")
+            print("No name")
         print(func.func)
         print(func.args)
         print(func.keywords)
@@ -27,3 +32,9 @@ test(3)
 # Now we create a function with smaller args than the original test function.
 test_with_fixed_a = functools.partial(test, 1)
 test_with_fixed_a(b=12)
+
+# Now we create a function with smaller args than the original test function.
+test_with_fixed_a_and_update_wrapper = functools.partial(test, 100)
+functools.update_wrapper(test_with_fixed_a_and_update_wrapper, test)
+show_details("test_with_fixed_a_and_update_wrapper", test_with_fixed_a_and_update_wrapper, True)
+test_with_fixed_a(b=1200)
