12824aabd24d12c881bc38e8a12677d8c86e6934
sabya14 <sabya.nandy14@gmail.com>
2019-05-07 22:23:28 +0530

    5.1 Change git global

--- bit_manipulation/insertion.py
+++ bit_manipulation/insertion.py
@@ -6,7 +6,7 @@
 def insertions(n, m, i, j):
     """
     Eg - > In 10001010, put 101 in 4-7 position, thus returning 11011010
-    """
+    """ 
     m_shifted = m << i  # move m i places left to align with n
     right_mask = (1 << i) - 1  # produces 0...011
     left_mask = -1 << j + 1  # -1 is basically -1b1111111111111,<< j -> 11110000, if j is 4
