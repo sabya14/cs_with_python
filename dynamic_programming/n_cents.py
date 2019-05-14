daf2292ed495bcc07b02ffcdda2fbfa7e3007419
sabya14 <sabya.nandy14@gmail.com>
2019-05-14 08:13:59 +0530

    8.11 Cracking the coding interview, optimal solution for generating n cents from a list of cents.

--- dynamic_programming/n_cents.py
+++ dynamic_programming/n_cents.py
@@ -2,24 +2,29 @@
 8.11 Problem from cracking the coding interview
 https://practice.geeksforgeeks.org/problems/coin-change/0
 """
-from math import floor
 
 
-def n_cents(n, cent_list, index):
-    if index >= len(cent_list) - 1:
+def n_cents(n, cent_list, index, _dict):
+    if n == 0:
         return 1
+    if index > len(cent_list) - 1:
+        return 0
     cent = cent_list[index]
-    print("Cent", cent)
-    possible_way_to_use = 0
+    possible_way_to_use = n // cent
     result = 0
-    while possible_way_to_use * cent <= n:
+    while possible_way_to_use > -1:
         print(
             f"Adding {possible_way_to_use} cents of value {cent},"
             f" left value {n - (possible_way_to_use * cent)}")
-        result += n_cents(n - (possible_way_to_use * cent), cent_list, index + 1)
-        possible_way_to_use += 1
-    print("Returning of")
+        if not ((n - (possible_way_to_use * cent), index + 1) in _dict):
+            _dict[(n - (possible_way_to_use * cent), index + 1)] = n_cents(n - (possible_way_to_use * cent),
+                                                                                      cent_list, index + 1, _dict)
+        result += _dict[(n - (possible_way_to_use * cent), index + 1)]
+        possible_way_to_use -= 1
     return result
 
 
-print(n_cents(10, [25,10,5,1], 0))
+print(n_cents(4, [3, 2, 1], 0, {}))
+print('--------')
+print(n_cents(10, [6, 5, 3, 2], 0, {}))
+print(n_cents(11, [5, 2, 1], 0, {}))
