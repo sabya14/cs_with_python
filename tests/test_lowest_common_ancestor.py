a611a975943386108c0bbc4c738162045603ba0d
sabyasachi.nandy <sabya.nandy14@gmail.com>
2020-06-20 19:01:30 +0530

    LCA in recursive approach

---
+++ tests/test_lowest_common_ancestor.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+from trees.lowest_common_ancestor import BinarySearchTree, lca
+
+
+class Test(TestCase):
+    def test_lca(self):
+        tree = BinarySearchTree()
+        input_data = [4, 2, 3, 1, 7, 6]
+        for i in input_data:
+            tree.create(i)
+        lca_node = lca(tree.root, 1, 7)
+        self.assertEqual(lca_node.info, 4)
+
+    def test_lca_only_right_side_present(self):
+        tree = BinarySearchTree()
+        input_data = [1, 2, 3, 4, 5, 6]
+        for i in input_data:
+            tree.create(i)
+        lca_node = lca(tree.root, 5, 6)
+        self.assertEqual(5, lca_node.info)
--- trees/lowest_common_ancestor.py
+++ trees/lowest_common_ancestor.py
@@ -1,53 +1,46 @@
-# class Node:
-#     def __init__(self, info):
-#         self.info = info
-#         self.left = None
-#         self.right = None
-#         self.level = None
-#
-#     def __str__(self):
-#         return str(self.info)
-#
-#
-# class BinarySearchTree:
-#     def __init__(self):
-#         self.root = None
-#
-#     def create(self, val):
-#         if self.root == None:
-#             self.root = Node(val)
-#         else:
-#             current = self.root
-#
-#             while True:
-#                 if val < current.info:
-#                     if current.left:
-#                         current = current.left
-#                     else:
-#                         current.left = Node(val)
-#                         break
-#                 elif val > current.info:
-#                     if current.right:
-#                         current = current.right
-#                     else:
-#                         current.right = Node(val)
-#                         break
-#                 else:
-#                     break
-#
-#
-# # Enter your code here. Read input from STDIN. Print output to STDOUT
-# '''
-# class Node:
-#       def __init__(self,info):
-#           self.info = info
-#           self.left = None
-#           self.right = None
-#
-#
-#        // this is a node of the tree , which contains info as data, left , right
-# '''
-#
-#
-# def lca(root, v1, v2):
-# # Enter your code here
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        if self.root is None:
+            self.root = Node(val)
+        else:
+            current = self.root
+
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+def lca(root, v1, v2):
+    current = root
+    if v1 < current.info and v2 < current.info:
+        return lca(current.left, v1, v2)
+    elif v1 > current.info and v2 > current.info:
+        return lca(current.right, v1, v2)
+    else:
+        return current
